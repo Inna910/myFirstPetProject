2a75e75c56d182e005444dbf1ca08cebbf1828a8
Inna910 <innakosh910@gmail.com>
2023-10-01 23:24:28 +0200

    completely changed design

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
+// Import the functions you need from the SDKs you need
 
+function signIn() {
+  const login = document.getElementById("login");
+  const password = document.getElementById("password");
+  const currentUser = {
+    login: loginInput.value,
+    password: passwordInput.value,
+  };
 
-function signIn () {
-    const loginInput = document.getElementById('login');
-    const passwordInput = document.getElementById('password');
-    const currentUser = {
-        login: loginInput.value,
-        password: passwordInput.value}
-    // TODO: implement getting a user from the LocalStorage and checking the presence
-
-    if(false) {
-
-    } else {
-       createUser(currentUser) 
-    }
-}
-
-
-function createUser (user) {
-    const userString = JSON.stringify(user);
-    localStorage.setItem(user.login, userString)
 }
